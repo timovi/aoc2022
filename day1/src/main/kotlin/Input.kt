cbacb8b90f59e364a3dd8be7befada4a508dcb38
Timo Viisteensaari <timo.viisteensaari@solita.fi>
2022-12-01 22:33:41 +0200

    Restructured project

--- day1/src/main/kotlin/Input.kt
+++ day1/src/main/kotlin/Input.kt
@@ -1,22 +1,3 @@
-fun main() {
-    val totals = calculateTotals(input)
-    println("Part one: ${totals.first()}")
-    println("Part two: ${totals.take(3).sum()}")
-}
-
-fun calculateTotals(input: String) =
-    input.split("\n")
-        .fold(listOf(0)) { elves, caloriesStr ->
-            when (caloriesStr) {
-                "" -> elves + 0
-                else -> {
-                    val currentCalories = elves.last() + caloriesStr.toInt()
-                    elves.dropLast(1) + currentCalories
-                }
-            }
-        }
-        .sortedDescending()
-
 val input = """
 6758
 5199
