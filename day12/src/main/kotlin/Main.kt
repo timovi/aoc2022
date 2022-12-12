ee30c5cc7b21b7bbb81b1bb8839afd98371a8dc2
Timo Viisteensaari <timo.viisteensaari@solita.fi>
2022-12-12 22:52:18 +0200

    working puzzle1

--- day12/src/main/kotlin/Main.kt
+++ day12/src/main/kotlin/Main.kt
@@ -1,59 +1,54 @@
+import java.util.Deque
+
 data class Coordinate(val x: Int, val y: Int)
 data class Node(
+    val char: Char,
     val coordinate: Coordinate,
-    val nextCoordinates: List<Coordinate>,
-    val depth: Int)
+    val neighbours: List<Coordinate>,
+    val distance: Int,
+    val visited: Boolean)
 
 fun main() {
     val input = {}::class.java.getResource("input.txt").readText()
 
     val map = input.split("\r\n")
-    val start = findCoordinateOf('S', map)!!
 
-    //var foo = findPaths(map, emptyList(), start)
+    val allNodes = mutableMapOf<Coordinate, Node>()
+    allNodes.putAll(mapToNodes(map).map { it.coordinate to it })
+    val start = allNodes.values.first { it.char == 'S' }
 
-    println("Puzzle one: ${findPaths(map, setOf(start), listOf(start), 0)}")
+    println("Puzzle one: ${findPaths(allNodes, listOf(start))}")
     println("Puzzle two: ")
 }
 
-tailrec fun findPaths(map: List<String>, allVisited: Set<Coordinate>, current: List<Coordinate>, level: Int): Int {
-    if (current.any { it.isEnd(map) }) {
-        return level
-    }
+fun mapToNodes(map: List<String>) =
+    map.flatMapIndexed() { y, row -> row.mapIndexed { x, _ -> Coordinate(x, y).toNode(map) }}
 
-    val next = mutableListOf<Coordinate>()
-    current.map { coordinate ->
-        val up    = Coordinate(coordinate.x, coordinate.y-1)
-        val left  = Coordinate(coordinate.x-1, coordinate.y);
-        val down  = Coordinate(coordinate.x, coordinate.y+1);
-        val right = Coordinate(coordinate.x+1, coordinate.y);
+fun findPaths(allNodes: MutableMap<Coordinate, Node>, start: List<Node>) : Int {
+    var current = start.first()
+    val unvisited = mutableListOf<Node>()
+    unvisited.addAll(allNodes.values)
 
-        if (coordinate.canTravelTo(map, up) && !allVisited.contains(up)) {
-            next.add(up)
-        }
-        if (coordinate.canTravelTo(map, left) && !allVisited.contains(left)) {
-            next.add(left)
-        }
-        if (coordinate.canTravelTo(map, down) && !allVisited.contains(down)) {
-            next.add(down)
-        }
-        if (coordinate.canTravelTo(map, right) && !allVisited.contains(right)) {
-            next.add(right)
-        }
-    }
-
-    return findPaths(map, allVisited + current, next,level + 1)
-}
-
-fun findCoordinateOf(c: Char, map: List<String>): Coordinate? {
-    for (y in map.indices) {
-        for (x in map[y].indices) {
-            if (map[y][x] == c) {
-                return Coordinate(x, y)
+    while (unvisited.any()) {
+        current.neighbours.forEach { neighbour ->
+            val node = allNodes[neighbour]!!
+            val newDistance = Math.min(current.distance + 1, node.distance)
+            val newNode = Node(node.char, node.coordinate, node.neighbours, newDistance, false)
+            allNodes[neighbour] = newNode
+            if (unvisited.contains(node)) {
+                unvisited.remove(node)
+                unvisited.add(newNode)
             }
         }
+        if (current.isEnd()) {
+            return current.distance
+        } else {
+            unvisited.remove(current)
+            unvisited.sortBy { it.distance }
+            current = unvisited.first()
+        }
     }
-    return null
+    return 0
 }
 
 fun Coordinate.canTravelTo(map: List<String>, coordinate: Coordinate): Boolean {
@@ -72,5 +67,33 @@ fun Coordinate.canTravelTo(map: List<String>, coordinate: Coordinate): Boolean {
     }
 }
 
-fun Coordinate.isEnd(map: List<String>) =
-    map[this.y][this.x] == 'E'
+fun Coordinate.toNode(map: List<String>) : Node {
+    val next = mutableListOf<Coordinate>()
+
+    val up    = Coordinate(this.x, this.y-1)
+    val left  = Coordinate(this.x-1, this.y);
+    val down  = Coordinate(this.x, this.y+1);
+    val right = Coordinate(this.x+1, this.y);
+
+    if (this.canTravelTo(map, up)) {
+        next.add(up)
+    }
+    if (this.canTravelTo(map, left)) {
+        next.add(left)
+    }
+    if (this.canTravelTo(map, down)) {
+        next.add(down)
+    }
+    if (this.canTravelTo(map, right)) {
+        next.add(right)
+    }
+
+    val char = map[this.y][this.x]
+    val distance = when(char) {
+        'S' -> 0
+        else -> 999999
+    }
+    return Node(char,this, next, distance, false)
+}
+
+fun Node.isEnd() = this.char == 'E'
