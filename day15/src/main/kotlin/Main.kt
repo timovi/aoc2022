c88ba6a4e71b4f0fd992881ac5ab24a70059a842
Timo Viisteensaari <timo.viisteensaari@solita.fi>
2022-12-19 13:38:16 +0200

    day 15, puzzle 2

--- day15/src/main/kotlin/Main.kt
+++ day15/src/main/kotlin/Main.kt
@@ -1,30 +1,29 @@
 data class Coordinate(val x: Int, val y: Int)
-data class ScanRange(val y: Int, val xRange: IntRange)
-data class Sensor(
-    val position: Coordinate,
-    val closestBeacon: Coordinate,
-    val distance: Int
-)
+
+val scanRanges = mutableMapOf<Int, MutableList<IntRange>>()
+val beacons = mutableMapOf<Int, MutableList<Int>>()
 
 fun main() {
-    val input = {}::class.java.getResource("input2.txt").readText()
-    val sensors = parseSensors(input)
-
+    val input = {}::class.java.getResource("input.txt").readText()
     val inspectedRow = 2000000
-    println("Puzzle one: ${unknownXCoordinatesThatCannotContainBeacon(inspectedRow, sensors).size}")
+    parseSensors(input, inspectedRow, inspectedRow)
+
+    println("Puzzle one: ${unknownXCoordinatesThatCannotContainBeacon(inspectedRow).size}")
+
+    scanRanges.clear()
+    beacons.clear()
 
     val min = 0
-    val max = 20
-
-
-
-    println("Puzzle two: ")
+    val max = 4000000
+    parseSensors(input, min, max)
+    val signal = coordinateThatContainsDistressSignal(min..max)
+    println("Puzzle two: ${signal!!.x.toLong() * 4000000 + signal!!.y.toLong()}")
 }
 
-fun parseSensors(input: String) =
+fun parseSensors(input: String, minYForScanning: Int, maxYForScanning: Int) =
     input
         .split("\r\n")
-        .map {
+        .forEach {
             val match =
                 Regex("Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)").find(it)!!
             val (sensorX, sensorY, beaconX, beaconY) = match.destructured
@@ -32,51 +31,79 @@ fun parseSensors(input: String) =
             val beaconCoordinate = Coordinate(beaconX.toInt(), beaconY.toInt())
             val distance =
                 Math.abs(sensorCoordinate.x - beaconCoordinate.x) + Math.abs(sensorCoordinate.y - beaconCoordinate.y)
-            Sensor(sensorCoordinate, beaconCoordinate, distance)
+            addToScanRanges(sensorCoordinate, distance, minYForScanning, maxYForScanning)
+            addToBeacons(beaconCoordinate)
         }
 
-fun unknownXCoordinatesThatCannotContainBeacon(y: Int, sensors: List<Sensor>) : Set<Int> {
-    val coveringSensors = sensors.flatMap { it.coveredXCoordinatesIn(y) }.toSet()
+fun addToScanRanges(coordinate: Coordinate, distance: Int, minY: Int, maxY: Int) {
+    val minYForRange = Math.max(minY, coordinate.y - distance)
+    val maxYForRange = Math.min(maxY, coordinate.y + distance)
+    for (y in minYForRange .. maxYForRange) {
+        val xRange = coveredXCoordinatesIn(coordinate, distance, y)
 
-    val coveringBeacons = sensors.filter { it.closestBeacon.y == y }.map { it.closestBeacon.x }.toSet()
+        if (!xRange.isEmpty()) {
+            if (scanRanges.contains(y)) {
+                scanRanges[y]?.add(xRange)
+            } else {
+                scanRanges[y] = mutableListOf(xRange)
+            }
+        }
+    }
+}
+
+fun addToBeacons(coordinate: Coordinate) {
+    if (beacons.contains(coordinate.y)) {
+        beacons[coordinate.y]?.add(coordinate.x)
+    } else {
+        beacons[coordinate.y] = mutableListOf(coordinate.x)
+
+    }
+}
+
+fun unknownXCoordinatesThatCannotContainBeacon(y: Int) : Set<Int> {
+    val coveringSensors = scanRanges[y]!!.flatten().toSet()
+    val coveringBeacons = beacons[y]!!.toSet()
     return (coveringSensors - coveringBeacons)
 }
 
-/*fun xCoordinatesThatCanContainDistressSignal(coordinateRange: IntRange, sensors: List<Sensor>) : Set<Int> {
-    for (y in coordinateRange.start .. coordinateRange.endInclusive) {
-        val coveringSensors = sensors.map { it.coveredXCoordinatesIn(y) }
+fun coordinateThatContainsDistressSignal(coordinateRange: IntRange) : Coordinate? {
+    for (y in coordinateRange.first.. coordinateRange.last) {
+        val coveringSensors = scanRanges[y]!!.map { it.clamp(coordinateRange.first, coordinateRange.last) }
+        val endpoints = coveringSensors.flatMap { setOf(it.first, it.last) }.toSet().sorted()
 
-        coveringSensors.first().intersect()
+        if (endpoints.first() != coordinateRange.first) {
+            return Coordinate(coordinateRange.first, y)
+        } else if (endpoints.last() != coordinateRange.last) {
+            return Coordinate(coordinateRange.last, y)
+        } else {
+            val notKnownXCoordinate = endpoints
+                .drop(1)
+                .dropLast(1)
+                .map { it + 1 }
+                .firstOrNull {endpoint ->
+                    coveringSensors.none { it.contains(endpoint) }
+                }
 
-        for (x in coordinateRange.start .. coordinateRange.endInclusive)
+            if (notKnownXCoordinate != null) {
+                return Coordinate(notKnownXCoordinate, y)
+            }
+        }
     }
+    return null
+}
 
-    val coveringBeacons = sensors.filter { it.closestBeacon.y == y }.map { it.closestBeacon.x }.toSet()
-    return (coveringSensors - coveringBeacons)
-}*/
-
-fun Sensor.coveredXCoordinatesIn(y: Int): IntRange {
-    val yDifference = Math.abs(this.position.y - y)
-    if (this.distance < yDifference) {
+fun coveredXCoordinatesIn(position: Coordinate, distance: Int, y: Int): IntRange {
+    val yDifference = Math.abs(position.y - y)
+    if (distance < yDifference) {
             return IntRange.EMPTY
     }
 
     val xDifference = distance - yDifference
-    return this.position.x - xDifference .. this.position.x + xDifference
+    return position.x - xDifference .. position.x + xDifference
 }
 
-fun IntRange.except(other: IntRange) : List<IntRange> {
-    return if (this == other ) {
-        listOf(IntRange.EMPTY)
-    } else if (this.start > other.endInclusive) {
-        listOf(this)
-    } else if (this.start == other.endInclusive) {
-        listOf(IntRange(this.start+1, this.end))
-    } else if (this.start < other.endInclusive && this.start >= other.start ) {
-        listOf(IntRange(other.endInclusive + 1, this.endInclusive))
-    } else if (this.endInclusive >= other.start) {
-        listOf(IntRange(this.start, other.start - 1))
-    } else {
-        listOf(this)
-    }
+fun IntRange.clamp(min: Int, max: Int): IntRange {
+    return IntRange(
+        Math.max(min, this.first),
+        Math.min(max, this.last))
 }
